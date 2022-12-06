7e2c0ae1d6e905853dc10ca9a70b07b99e0b074f
dashbah <dsbakhankova@edu.hse.ru>
2022-12-06 14:53:21 +0300

    pw5

--- dsbakhankovaPW5/dsbakhankovaPW3/AppDelegate.swift
+++ dsbakhankovaPW5/dsbakhankovaPW3/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     UINavigationController(rootViewController:
                             WelcomeViewController())
     // swiftlint:disable line_length
+    // 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
         window.rootViewController = navigationController
